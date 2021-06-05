3df5e859aac82bf86cf631d11191dc0facbc6e39
nvdat <datdhvinh@gmail.com>
2021-06-05 18:24:15 +0700

    update sql queries and init role queries

--- DB/BLOG_DEV_DB/01.init_table.sql
+++ DB/BLOG_DEV_DB/01.init_table.sql
@@ -1,4 +1,4 @@
-# use blog;
+-- use blog;
 
 -- ----------------------------
 -- Table structure for blog_module
@@ -25,8 +25,8 @@ DROP TABLE IF EXISTS `blog`.`blog_role`;
 CREATE TABLE `blog`.`blog_role` (
   `role_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'user role id',
   `role_name` varchar(50) NOT NULL COMMENT 'role name',
-  `role_right` varchar(5) NOT NULL COMMENT 'role right, MC-Macker and Checker flow(As Admin); NA-No need MC flow; SAD-Supper admin',
-  `role_status` char(1) DEFAULT '1' NOT NULL COMMENT 'role status: Active, Suspend, Delete',
+  `role_right` varchar(5) NOT NULL DEFAULT 'NA' COMMENT 'role right, MC-Macker and Checker flow(As Admin); NA-No need MC flow; SAD-Supper admin',
+  `role_status` char(1) DEFAULT '1' NOT NULL COMMENT 'role status: 0-Delete, 1-Exist',
   `is_edit` char(2) NOT NULL DEFAULT '0' COMMENT 'role is edited? 0: no-edit; 1: edit',
   `created_by` varchar(50) DEFAULT 'system' COMMENT 'created by',
   `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -43,10 +43,10 @@ DROP TABLE IF EXISTS `blog`.`blog_access_right`;
 CREATE TABLE `blog`.`blog_access_right` (
   `module_code` varchar(10) NOT NULL COMMENT 'module code',
   `role_id` int(11) NOT NULL COMMENT 'role id',
-  `access_right` varchar(2) NOT NULL DEFAULT 'NA' COMMENT 'access right: NA-No Access; RO-Read Only; RW-Read & Write; MC-Maker or Checker',
-  `created_by` varchar(50) DEFAULT 'system' COMMENT 'creator',
+  `access_right` varchar(2) NOT NULL DEFAULT 'RO' COMMENT 'access right: NA-No Access; RO-Read Only; RW-Read & Write',
+  `created_by` varchar(50) DEFAULT 'system' COMMENT 'create by',
   `created_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_by` varchar(50) DEFAULT 'system' COMMENT 'updator',
+  `updated_by` varchar(50) DEFAULT 'system' COMMENT 'update by',
   `updated_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   UNIQUE KEY `right_module_index` (`module_code`,`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Blog access right table';
@@ -81,23 +81,149 @@ CREATE TABLE `blog`.`blog_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT 'User table';
 
 -- ----------------------------
--- Table structure for blog_comment
+-- Table structure for blog_post
 -- ----------------------------
-DROP TABLE IF EXISTS `blog`.`blog_comment`;
-CREATE TABLE `blog`.`blog_comment` (
-  `comment_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'comment id, auto increment',
-  `comment_content` text NOT NULL COMMENT 'comment content',
-  `user_uuid` varchar(50) NOT NULL COMMENT 'user id of comment',
-  `movie_id` bigint(20) NOT NULL COMMENT 'movie id of comment',
-  `is_edit` char(2) NOT NULL DEFAULT '0' COMMENT 'is edit: 0-not edit 1-edit',
-  `event_id` bigint(20) DEFAULT NULL,
-  `created_by` varchar(50) DEFAULT 'system' COMMENT 'created by',
-  `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_by` varchar(50) DEFAULT 'system' COMMENT 'update by',
-  `updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `blog`.`blog_post`;
+CREATE TABLE `blog`.`blog_post`
+(
+    `post_id`          bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'Post id, auto increment',
+    `author_id`        bigint(20)   NOT NULL COMMENT 'Author id for per post',
+    `parent_id`        varchar(100)          DEFAULT NULL COMMENT 'Parent id, as relationship of post',
+    `title`            varchar(200) NOT NULL COMMENT 'Post\'s title',
+    `content`          longtext     NOT NULL COMMENT 'Post\'s content',
+    `summary`          tinytext              DEFAULT NULL COMMENT 'Post\'s summary',
+    `post_status`      char(1)      NOT NULL DEFAULT '0' COMMENT 'Post\'s status; Ex: published-1, not publish-0',
+    `meta_title`       varchar(200)          DEFAULT NULL COMMENT 'Post\'s meta title (title of web) for SEO',
+    `slug`             varchar(100)          DEFAULT NULL COMMENT 'It\'s an important part of URL for SEO',
+    `post_has_article` char(1)      NOT NULL DEFAULT '0' COMMENT 'Post has article; 0-no article, 1-has article',
+    `article_title`    varchar(200)          DEFAULT NULL COMMENT 'Article\'s title',
+    `article_content`  LONGTEXT              DEFAULT NULL COMMENT 'Article\'s content',
+    `created_by`       varchar(50)           DEFAULT 'system' COMMENT 'created by',
+    `created_date`     datetime              DEFAULT CURRENT_TIMESTAMP,
+    `updated_by`       varchar(50)           DEFAULT 'system' COMMENT 'update by',
+    `updated_date`     datetime              DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`post_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT 'Blog post table';
+
+-- ---------------------------------
+-- Table structure for blog_category
+-- ---------------------------------
+DROP TABLE IF EXISTS `blog`.`blog_category`;
+CREATE TABLE `blog`.`blog_category`
+(
+    `category_id`  bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'Category id, auto increment',
+    `title`        varchar(200) NOT NULL COMMENT 'Category\'s title',
+    `meta_title`   tinytext     DEFAULT NULL COMMENT 'Category\'s meta title (title of web) for SEO',
+    `slug`         varchar(100) DEFAULT NULL COMMENT 'It\'s an important part of URL for SEO',
+    `content`      longtext     NOT NULL COMMENT 'Category\'s content',
+    `created_by`   varchar(50)  DEFAULT 'system' COMMENT 'created by',
+    `created_date` datetime     DEFAULT CURRENT_TIMESTAMP,
+    `updated_by`   varchar(50)  DEFAULT 'system' COMMENT 'update by',
+    `updated_date` datetime     DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`category_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT 'Blog category table';
+
+-- --------------------------------------
+-- Table structure for blog_post_category
+-- --------------------------------------
+DROP TABLE IF EXISTS `blog`.`blog_post_category`;
+CREATE TABLE `blog`.`blog_post_category`
+(
+    `post_id`      bigint(20) NOT NULL COMMENT 'Post id, auto increment',
+    `category_id`  bigint(20) NOT NULL COMMENT 'Category id, auto increment',
+    `created_by`   varchar(50) DEFAULT 'system' COMMENT 'created by',
+    `created_date` datetime    DEFAULT CURRENT_TIMESTAMP,
+    `updated_by`   varchar(50) DEFAULT 'system' COMMENT 'update by',
+    `updated_date` datetime    DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`post_id`, `category_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT 'Blog post category table';
 
 -- ----------------------------
--- Table structure for blog_category
+-- Table structure for blog_tag
 -- ----------------------------
+DROP TABLE IF EXISTS `blog`.`blog_tag`;
+CREATE TABLE `blog`.`blog_tag`
+(
+    `tag_id`       bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'Tag id, auto increment',
+    `post_id`      bigint(20)   NOT NULL COMMENT 'Post id, auto increment',
+    `title`        varchar(200) NOT NULL COMMENT 'Tag\'s title',
+    `meta_title`   tinytext     DEFAULT NULL COMMENT 'Tag\'s meta title (title of web) for SEO',
+    `slug`         varchar(100) DEFAULT NULL COMMENT 'It\'s an important part of URL for SEO',
+    `content`      longtext     NOT NULL COMMENT 'Tag\'s content',
+    `created_by`   varchar(50)  DEFAULT 'system' COMMENT 'created by',
+    `created_date` datetime     DEFAULT CURRENT_TIMESTAMP,
+    `updated_by`   varchar(50)  DEFAULT 'system' COMMENT 'update by',
+    `updated_date` datetime     DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`tag_id`),
+    UNIQUE KEY `post_id` (`post_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT 'Blog category table';
+
+-- --------------------------------
+-- Table structure for blog_comment
+-- --------------------------------
+DROP TABLE IF EXISTS `blog`.`blog_comment`;
+CREATE TABLE `blog`.`blog_comment`
+(
+    `comment_id`     bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'comment id, auto increment',
+    `post_id`        bigint(20) NOT NULL COMMENT 'Post id, auto increment',
+    `parent_id`      bigint(20)          DEFAULT NULL COMMENT 'Parent id of comment for reply comment',
+    `content`        text       NOT NULL COMMENT 'Comment content',
+    `comment_status` char(1)    NOT NULL DEFAULT '0' COMMENT '0-not published; 1-published',
+    `created_by`     varchar(50)         DEFAULT 'system' COMMENT 'created by',
+    `created_date`   datetime            DEFAULT CURRENT_TIMESTAMP,
+    `updated_by`     varchar(50)         DEFAULT 'system' COMMENT 'update by',
+    `updated_date`   datetime            DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`comment_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+-- -----------------------------
+-- Table structure for blog_meta
+-- -----------------------------
+DROP TABLE IF EXISTS `blog`.`blog_meta`;
+CREATE TABLE `blog`.`blog_meta`
+(
+    `meta_id`      bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'comment id, auto increment',
+    `post_id`      bigint(20)   NOT NULL COMMENT 'Post id, auto increment',
+    `key`          varchar(100) NOT NULL COMMENT 'Key of post',
+    `content`      text         NOT NULL COMMENT 'Content of key',
+    `created_by`   varchar(50) DEFAULT 'system' COMMENT 'created by',
+    `created_date` datetime    DEFAULT CURRENT_TIMESTAMP,
+    `updated_by`   varchar(50) DEFAULT 'system' COMMENT 'update by',
+    `updated_date` datetime    DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`meta_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+-- ---------------------------------------
+-- Table structure for blog_user_event_log
+-- ---------------------------------------
+DROP TABLE IF EXISTS `blog`.`blog_user_event_log`;
+CREATE TABLE `blog`.`blog_user_event_log`
+(
+    `log_id`        bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `user_uuid`     varchar(50)         DEFAULT NULL COMMENT 'id user',
+    `post_id`       bigint(20) NOT NULL COMMENT 'Post id, auto increment',
+    `event_type`    char(2)    NOT NULL DEFAULT '00' COMMENT '00-Others, 01-Successful, 02-Failed, 03-OTP error, 04-Checking error, 05-RAMCI Litigation error, 06-IRL error, 07-AML error, 08-Login/Logout, 09-Web service call',
+    `event_name`    text       NOT NULL COMMENT 'event name',
+    `details`       text COMMENT 'Event details',
+    `event_session` char(2)             DEFAULT '01' COMMENT '01-session current, 02-session past',
+    `created_by`    varchar(45)         DEFAULT 'system' COMMENT 'created by',
+    `updated_by`    varchar(45)         DEFAULT 'system' COMMENT 'updated by',
+    `created_date`  datetime            DEFAULT CURRENT_TIMESTAMP COMMENT 'created date',
+    `updated_date`  datetime            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Updated date',
+    PRIMARY KEY (`log_id`),
+    KEY `blog_user_event_log_movie_id_index` (`user_uuid`, `post_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT ='Blog User event logs';
--- DB/BLOG_DEV_DB/02.init_role.sql
+++ DB/BLOG_DEV_DB/02.init_role.sql
@@ -1,42 +1,47 @@
+TRUNCATE TABLE `blog`.`blog_module`;
+TRUNCATE TABLE `blog`.`blog_role`;
+TRUNCATE TABLE `blog`.`blog_user`;
+TRUNCATE TABLE `blog`.`blog_access_right`;
 
 -- init the super user role and default 4 roles
-INSERT INTO `blog`.`blog_role` (`role_id`, `role_code`, `role_name`, `role_right`, `role_status`) VALUES ('1', 'SUPER_ADMIN', 'Super Admin', 'NA', '1');
-INSERT INTO `blog`.`blog_role` (`role_id`, `role_code`, `role_name`, `role_right`, `role_status`) VALUES ('2', 'ADMIN', 'Admin', 'MC', '1');
-INSERT INTO `blog`.`blog_role` (`role_id`, `role_code`, `role_name`, `role_right`, `role_status`) VALUES ('3', 'USER_VIEW', 'View or read only', 'MC', '1');
-INSERT INTO `blog`.`blog_role` (`role_id`, `role_code`, `role_name`, `role_right`, `role_status`) VALUES ('4', 'USER_EDIT', 'Edit or update', 'MC', '1');
-INSERT INTO `blog`.`blog_role` (`role_id`, `role_code`, `role_name`, `role_right`, `role_status`) VALUES ('5', 'USER_CREATE', 'Create new record', 'MC', '1');
+INSERT INTO `blog`.`blog_role` (`role_name`, `role_right`) VALUES ('Super Admin', 'SAD');
+INSERT INTO `blog`.`blog_role` (`role_name`, `role_right`) VALUES ('Maker or checker as Admin', 'MC');
+INSERT INTO `blog`.`blog_role` (`role_name`, `role_right`) VALUES ('No need', 'NA');
 
--- init the blog_role_detail
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('1', 'Create post', 'CREATE', '1', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('2', 'Edit post', 'EDIT', '1', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('3', 'Delete post', 'DELETE', '1', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('4', 'View post', 'VIEW', '1', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('5', 'Create post', 'CREATE', '2', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('6', 'Edit post', 'EDIT', '2', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('7', 'Delete post', 'DELETE', '2', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('8', 'View post', 'VIEW', '2', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('9', 'Create post', 'CREATE', '3', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('10', 'Edit post', 'EDIT', '3', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('11', 'Delete post', 'DELETE', '3', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('12', 'View post', 'VIEW', '3', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('13', 'Create post', 'CREATE', '4', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('14', 'Edit post', 'EDIT', '4', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('15', 'Delete post', 'DELETE', '4', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('16', 'View post', 'VIEW', '4', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('17', 'Create post', 'CREATE', '5', '1');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('18', 'Edit post', 'EDIT', '5', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('19', 'Delete post', 'DELETE', '5', '0');
-INSERT INTO `blog`.`blog_role_detail` (`role_detail_id`, `action_name`, `action_code`, `role_id`, `action_status`) VALUES ('20', 'View post', 'VIEW', '5', '1');
+-- init the blog_module
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('ue', 'User events', '1');
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('rae', 'Role access events', '2');
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('pe', 'Post events', '3');
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('cte', 'Category events', '4');
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('cme', 'Comment events', '5');
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('tge', 'Tag events', '6');
+INSERT INTO `blog`.`blog_module` (`module_code`, `module_name`, `sort`) VALUES ('mte', 'Meta events', '7');
 
--- init the super user //pass: 123456
-INSERT INTO `blog`.`blog_user` (user_uuid, full_name, email, username, password, cf_password, birthday, phone_number, gender) VALUES ('ed2f8903c46d47658d9d9e1c06caeb70', 'Nguyen Van Dat', 'datdhvinh@gmail.com', 'Kira', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1995-08-28', '0967838708', 'Male');
-INSERT INTO `blog`.`blog_user` (user_uuid, full_name, email, username, password, cf_password, birthday, phone_number, gender) VALUES ('ed2f8903c46d47658d9d9e1c06caeb71', 'Nguyen Manh Thao', 'manhthao@gmail.com', 'NMT', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1995-12-28', '0967521121', 'Male');
-INSERT INTO `blog`.`blog_user` (user_uuid, full_name, email, username, password, cf_password, birthday, phone_number, gender) VALUES ('ed2f8903c46d47658d9d9e1c06caeb72', 'Tran Xuan Chinh', 'chinhtran@gmail.com', 'CTX', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1994-01-16', '0952228708', 'Male');
-INSERT INTO `blog`.`blog_user` (user_uuid, full_name, email, username, password, cf_password, birthday, phone_number, gender) VALUES ('ed2f8903c46d47658d9d9e1c06caeb73', 'Nguyen Trong Cuong', 'trongcuong@gmail.com', 'Cuong', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1995-06-06', '0985554701', 'Male');
+-- init the blog_access_right
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('ue', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('rae', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('pe', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('cte', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('cme', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('tge', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('mte', '1', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('ue', '2', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('rae', '2', 'RO');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('pe', '2', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('cte', '2', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('cme', '2', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('tge', '2', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('mte', '2', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('ue', '3', 'NA');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('rae', '3', 'NA');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('pe', '3', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('cte', '3', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('cme', '3', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('tge', '3', 'RW');
+INSERT INTO `blog`.`blog_access_right` (`module_code`, `role_id`, `access_right`) VALUES ('mte', '3', 'NA');
 
--- init the blog_user_role
-INSERT INTO `blog`.`blog_user_role` (user_role_id, user_uuid, role_id, licensed) VALUES ('1', 'ed2f8903c46d47658d9d9e1c06caeb70', '1', '1');
-INSERT INTO `blog`.`blog_user_role` (user_role_id, user_uuid, role_id, licensed) VALUES ('2', 'ed2f8903c46d47658d9d9e1c06caeb70', '2', '1');
-INSERT INTO `blog`.`blog_user_role` (user_role_id, user_uuid, role_id, licensed) VALUES ('3', 'ed2f8903c46d47658d9d9e1c06caeb71', '3', '1');
-INSERT INTO `blog`.`blog_user_role` (user_role_id, user_uuid, role_id, licensed) VALUES ('4', 'ed2f8903c46d47658d9d9e1c06caeb72', '4', '1');
-INSERT INTO `blog`.`blog_user_role` (user_role_id, user_uuid, role_id, licensed) VALUES ('5', 'ed2f8903c46d47658d9d9e1c06caeb73', '5', '1');
+-- init the user //pass: 123456a@  or 123456
+INSERT INTO `blog`.`blog_user` (`user_uuid`, `role_id`, `full_name`, `email`, `username`, `password`, `cf_password`, `birthday`, `phone_number`, `gender`) VALUES ('ed2f8903c46d47658d9d9e1c06caeb70', '1', 'Nguyen Van Dat', 'datdhvinh@gmail.com', 'Kira', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1995-08-28', '0967838708', 'Male');
+INSERT INTO `blog`.`blog_user` (`user_uuid`, `role_id`, `full_name`, `email`, `username`, `password`, `cf_password`, `birthday`, `phone_number`, `gender`) VALUES ('ed2f8903c46d47658d9d9e1c06caeb71', '2', 'Nguyen Manh Thao', 'manhthao@gmail.com', 'NMT', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1995-12-28', '0967521121', 'Male');
+INSERT INTO `blog`.`blog_user` (`user_uuid`, `role_id`, `full_name`, `email`, `username`, `password`, `cf_password`, `birthday`, `phone_number`, `gender`) VALUES ('ed2f8903c46d47658d9d9e1c06caeb72', '2', 'Tran Xuan Chinh', 'chinhtran@gmail.com', 'CTX', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1994-01-16', '0952228708', 'Male');
+INSERT INTO `blog`.`blog_user` (`user_uuid`, `role_id`, `full_name`, `email`, `username`, `password`, `cf_password`, `birthday`, `phone_number`, `gender`) VALUES ('ed2f8903c46d47658d9d9e1c06caeb73', '3', 'Nguyen Trong Cuong', 'trongcuong@gmail.com', 'Cuong', '$2a$10$XMe4HY4TmmSXJ.Ih8EHt8eb4tTsed6JSow5vCntfSbwRhFaHL7Qn.','$2a$10$JMPP2gHz/khRBcQaNRG4sOq6mFKgFCxTxp7ogDZvyGeIioM3OmUoe', '1995-06-06', '0985554701', 'Female');
